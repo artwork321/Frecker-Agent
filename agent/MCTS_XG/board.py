f4637e909e91db23e0cf84546e383b87b39dc495
Kylie <thuylinh1@student.unimelb.edu.au>
2025-05-12 08:31:56 +0000

    clean codes

--- agent/MCTS_XG/board.py
+++ agent/MCTS_XG/board.py
@@ -1,20 +1,6 @@
 import numpy as np
 from referee.game import Direction
 
-'''
-Author: Eric P. Nichols
-Date: Feb 8, 2008.
-Modified by: Kylie Le 
-Board class.
-Board data:
-  1=red, -1=blue, 0=empty, 2=lily_pad
-  first dim is row , 2nd is column:
-     pieces[1][7] is the square in row 2,
-     at the opposite end of the board in column 8.
-Squares are stored and manipulated as (x,y) tuples.
-x is the row, y is the column.
-'''
-
 RED = 1
 BLUE = -1
 PAD = 2
@@ -57,8 +43,6 @@ class Board():
                             (0, 2)]} # right
 
     __goal_rows = {1: 7, -1: 0}
-    __player_goal_row = 7
-    __opponent_goal_row = 0
 
     def __init__(self, n):
         "Set up initial board configuration."
@@ -88,13 +72,11 @@ class Board():
             self.pieces[self.n-1][i] = -1
             self.player_cells[-1].append((self.n-1, i))
 
-    # add [][] indexer syntax to the Board
     def __getitem__(self, index): 
         return self.pieces[index]
     
     def setPieces(self, board):
         self.pieces = board
-        # print(f"setPieces broad:\n {board}")
 
         self.player_cells[1] = []
         self.player_cells[-1] = []
@@ -116,26 +98,11 @@ class Board():
     def count_players_at_goal(self, color, is_player_move):
         """Counts the # pieces that have reached the goal of the given color 
         (1 for red, -1 for blue)"""
-        # print(f"color: {color}")
-        # if is_player_move:
-        #     x = type(self).__player_goal_row
-        # else:
-        #     x = type(self).__opponent_goal_row
         count = 0
         x = type(self).__goal_rows[color]
         for y in range(self.n):
-            # if is_player_move:
-                if self.pieces[x][y] == color:
-                    count += 1
-            # else:
-            #     if self.pieces[x][y] == color:
-            #         count += 1
-        # for player_cell in self.player_cells[color]:
-        #     if player_cell[0] == goal_row:
-        #             count += 1
-        # print(f"count players at goal:\n {self.pieces}")
-        # print(f"color: {color}; player's move? {is_player_move}; count: {count}")
-        # import pdb; pdb.set_trace()
+            if self.pieces[x][y] == color:
+                count += 1
         return count
 
     def switch_perspectives(self):
@@ -144,15 +111,6 @@ class Board():
                 x, y = player_cell
                 self.pieces[x][y] = -color
         self.pieces = np.flipud(self.pieces)
-
-        # self.player_cells[RED] = []
-        # self.player_cells[BLUE] = []
-        # for x in range(self.n):
-        #     for y in range(self.n):
-        #         if self.pieces[x][y] == RED:
-        #             self.player_cells[RED].append((x, y))
-        #         elif self.pieces[x][y] == BLUE:
-        #             self.player_cells[BLUE].append((x, y))
 
         temp = self.player_cells[1]
         self.player_cells[1] = self.player_cells[-1]
@@ -170,7 +128,6 @@ class Board():
         for player_cell in self.player_cells[color]:
             newmoves = self.get_moves_for_square(player_cell, multi_jump)
             moves += newmoves
-        # import pdb; pdb.set_trace()
         return moves
 
     def has_legal_moves(self, color):
@@ -185,9 +142,7 @@ class Board():
             for direction in type(self).__all_directions:
                 x, y = self._get_move_endpoint(player_cell, direction)
                 if self._is_valid_square(x, y) and self.pieces[x][y] == EMPTY:
-                    # print(f"{color} has legal grow")
                     return True
-        # print(f"{color} doesn't have legal grow")
         return False       
 
     def get_moves_for_square(self, square, multi_jump=False):
@@ -204,21 +159,10 @@ class Board():
         # search all possible directions.
         return self._discover_moves(square, color, multi_jump)
 
-    def execute_move(self, origin, direction, color): # TODO handle multi-jumps; for now, assume game only allows max 1 jump
+    def execute_move(self, origin, direction, color): 
         """Perform the given move on the board.
         color gives the color of the piece to play (1=red,-1=blue)
         """
-        # switch = False
-        # if color < 0: 
-        #     self.switch_perspectives()
-        #     switch = True
-        #     color = -color
-        #     direction = type(self).__opp_direction[direction]
-
-        # if color < 0:
-        #     direction = type(self).__opp_direction[direction]
-        # print(f"player cells: {self.player_cells}")
-        # print(f"board:\n {self.pieces}")
 
         x, y = origin
         self.pieces[x][y] = EMPTY
@@ -232,17 +176,10 @@ class Board():
             import pdb; pdb.set_trace()
 
         self.pieces[x][y] = color
-        # import pdb; pdb.set_trace()
-        # if switch: import pdb; pdb.set_trace()
         if origin not in self.player_cells[color]:
             import pdb; pdb.set_trace()
         self.player_cells[color].remove(origin)
         self.player_cells[color].append((x, y))
-        
-        # print(f"origin: {origin}")
-        # print(f"color: {color}")
-        # print(f"player cells: {self.player_cells}")
-        # if switch: self.switch_perspectives()
 
         if not self._is_valid_square(x, y):
             import pdb; pdb.set_trace()
@@ -269,14 +206,11 @@ class Board():
                 x, y = self._get_move_endpoint(player_cell, direction)
                 if self._is_valid_square(x, y) and self.pieces[x][y] == EMPTY:
                     self.pieces[x][y] = PAD
-        # import pdb; pdb.set_trace()
         
     def execute_multiple_moves(self, origin, directions, is_red, color=-1):
         origin = (origin.r, origin.c)
         if is_red:
             origin = ((N_BOARD - 1) - origin[0], origin[1])
-            
-        # print(f"origin {origin}; is red {is_red}")
             
         if isinstance(directions, Direction):
             direction = (directions.r, directions.c)
@@ -310,12 +244,8 @@ class Board():
             if self._is_valid_square(x, y) and self.pieces[x][y] == PAD:
                 moves.append([(origin, direction)])
 
-        # print(f"moves wo jumps: {moves}")
-        # explore possible jumps
         moves += self._discover_jumps(origin, color, multi_jump)
 
-        # print(f"board:\n {self.pieces}")
-        # print(f"jumps: {moves}")
         return moves
 
     def _discover_jumps(self, origin, color, multi_jump=False, last_dir=None):
@@ -340,7 +270,6 @@ class Board():
     # get endpoint of a single move in the specified direction 
     def _get_move_endpoint(self, origin, direction):
         return list(map(sum, zip(origin, direction)))
-        # endpoint = (origin[0]+direction[0], origin[1]+direction[1])
     
     def _get_multi_jump_endpoint(self, origin, directions, is_red):
         x, y = origin
--- agent/MCTS_XG/game.py
+++ agent/MCTS_XG/game.py
@@ -42,37 +42,16 @@ class FreckersGame():
         +1: "R"
     }
 
-    @staticmethod
-    def getSquarePiece(piece):
-        return FreckersGame.square_content[piece]
-
     def __init__(self, n):
         self.n = n
 
-    def getInitBoard(self):
-        # return initial board (numpy board)
-        b = Board(self.n)
-        return np.array(b.pieces)
-
-    def getBoardSize(self):
-        # (a,b) tuple
-        return (self.n, self.n)
-
-    def getActionSize(self):
-        # return number of actions
-        return self.n*self.n*N_MOVES + 1
-        # #start_squares * #possible_directions (move) + grow
-
     def getNextState(self, board, player, action):
-        # if player takes action on board, return next (board,player)
-        # action must be a valid move
         b = Board(self.n)
         b.setPieces(np.copy(board))
         if len(b.player_cells[1]) != N_FROGS or len(b.player_cells[-1]) != N_FROGS:
             import pdb; pdb.set_trace()
 
         if action[0] == GROW_ACTION:
-            # print(f"executing grow for player {player}")
             b.execute_grow(player)
             return (b.pieces, -player)
 
@@ -80,8 +59,6 @@ class FreckersGame():
             action = action[0]
             origin = (int(int(action/N_MOVES)/self.n), int(action/N_MOVES)%self.n)
             direction = type(self).__id_direction[action%N_MOVES]
-            # print(f"origin {origin}")
-            # print(f"direction: {direction}")
             if player < 0:
                 origin = ((N_BOARD - 1) - origin[0], origin[1])
                 direction = FreckersGame.__opp_direction[direction]
@@ -117,16 +94,7 @@ class FreckersGame():
 
         return is_jump_over_teammate, is_jump_over_opp
 
-    def switchAction(action):
-        origin = (int(int(action/N_MOVES)/N_BOARD), int(action/N_MOVES)%N_BOARD)
-        direction = FreckersGame.__id_direction[action%N_MOVES]
-        x, y = (N_BOARD - 1) - origin[0], origin[1]
-        direction_idx = FreckersGame.__direction_ids[FreckersGame.__opp_direction[direction]]
-        # if x <0: import pdb; pdb.set_trace()
-        return N_BOARD*N_MOVES*x + N_MOVES*y + direction_idx
-
     def getValidMoves(self, board, player=1):
-        # return a fixed size binary vector
         valids = []
         b = Board(self.n)
         b.setPieces(np.copy(board))
@@ -135,30 +103,17 @@ class FreckersGame():
             valids.append(tuple([GROW_ACTION]))
 
         legalMoves = b.get_legal_moves(player, multi_jump=True) 
-        # print(f"legal moves: {legalMoves}")
         for origin_direction_list in legalMoves:
             action_indices = []
             for origin, direction in origin_direction_list:
-                # print(f"origin: {origin}")
-                # print(f"directions: {directions}")
                 x, y = origin
-                # import pdb; pdb.set_trace()
                 direction_idx = type(self).__direction_ids[direction] 
                 action_indices.append(self.n*N_MOVES*x + N_MOVES*y + direction_idx)
             valids.append(tuple(action_indices))
 
         return valids
 
-    def _get_jump_dir_endpoint(self, jump_action):
-        origin = (int(int(jump_action/N_MOVES)/self.n), int(jump_action/N_MOVES)%self.n)
-        direction = type(self).__id_direction[jump_action%N_MOVES]
-        jump_vector = (direction[0]*2, direction[1]*2)
-        return direction, list(map(sum, zip(origin, jump_vector)))
-
     def getGameEnded(self, board, player, n_turns=0, max_turn=MAX_N_TURNS):
-        # return 0 if not ended, 1 if player 1 won, -1 if player 1 lost
-        # player = 1
-        # print(f"player: {player}")
         b = Board(self.n)
         b.setPieces(np.copy(board))
         
@@ -178,31 +133,12 @@ class FreckersGame():
         b = Board(self.n)
         b.setPieces(np.copy(board))
 
-        # return state if player==1, else return -state if player==-1
         if player < 0:
             b.switch_perspectives()
 
-        # print(f"getCanonicalForm board\n: {np.array(b.pieces)}")
         return np.array(b.pieces)
 
-    def getSymmetries(self, board, pi):
-        # reshape the policy vector to a 2D move policy grid
-        symmetries = []
-
-        # original
-        symmetries.append((board.copy(), pi.copy()))
-
-        # pi_board = np.reshape(pi[:-1], (self.n, self.n*N_MOVES))  # assuming last entry is for "grow"
-        # # horizontal flip
-        # board_flipped = np.fliplr(board)
-        # pi_flipped = np.fliplr(pi_board)
-        # pi_flipped = list(pi_flipped.ravel()) + [pi[-1]]  # preserve final action
-        # symmetries.append((board_flipped, pi_flipped))
-
-        return symmetries
-
     def stringRepresentation(self, board):
-        # print(f"broad:\n {board}")
         return board.tostring()
 
     def stringRepresentationReadable(self, board):
@@ -213,20 +149,3 @@ class FreckersGame():
         b = Board(self.n)
         b.setPieces(np.copy(board))
         return b.countDiff(player)
-
-    @staticmethod
-    def display(board):
-        n = board.shape[0]
-        print("   ", end="")
-        for y in range(n):
-            print(y, end=" ")
-        print("")
-        print("-----------------------")
-        for y in range(n):
-            print(y, "|", end="")    # print the row #
-            for x in range(n):
-                piece = board[y][x]    # get the piece to print
-                print(FreckersGame.square_content[piece], end=" ")
-            print("|")
-
-        print("-----------------------")
