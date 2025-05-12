10d51c9c6b5a0e02443e2301e5b9efbd0eed3ce1
Lam Nguyen <hoangthanhla@student.unimelb.edu.au>
2025-05-12 19:38:14 +1000

    fix import

--- agent/MCTS_XG/mcts_cache.py
+++ agent/MCTS_XG/mcts_cache.py
@@ -1,4 +1,14 @@
-from agent.transposition_table import ZobristHashing
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+
+from transposition_table import ZobristHashing
 import numpy as np
 import json
 
